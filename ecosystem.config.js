f1a2b0b56545aeeb134e279e69ff3dfed9f2d4dc
TuanThanh2kk4 <tuanthanh2kk4@gmail.com>
2025-07-01 20:41:51 +0700

    refactor: comment out old EC2 deployment configuration in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,25 @@
 module.exports = {
   apps: [
+    // {
+    //   name: 'deploy-to-ec2-by-github-ci-cd',
+    //   script: 'docker',
+    //   args: 'compose up --build -d',
+    //   cwd: '/home/ubuntu/deploy-to-ec2-by-github-ci-cd/',
+    //   instances: 1,
+    //   autorestart: true,
+    //   watch: false,
+    //   max_memory_restart: '1G',
+    //   env: {
+    //     NODE_ENV: 'production',
+    //   },
+    // },
     {
-      name: 'deploy-to-ec2-by-github-ci-cd',
-      script: 'docker',
-      args: 'compose up --build -d',
+      name: 'deploy-to-aws',
+      script: 'npm',
+      args: 'run start:dev',
       cwd: '/home/ubuntu/deploy-to-ec2-by-github-ci-cd/',
-      instances: 1,
-      autorestart: true,
       watch: false,
-      max_memory_restart: '1G',
+      autorestart: true,
       env: {
         NODE_ENV: 'production',
       },
